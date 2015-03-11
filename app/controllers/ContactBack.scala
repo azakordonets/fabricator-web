1ecc39507fca4e6536a10441f0a8e491d7758448
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2015-03-11 21:41:24 +0100

    add contact controllers methods and first steps of internationalisation

--- app/controllers/ContactBack.scala
+++ app/controllers/ContactBack.scala
@@ -1,24 +1,70 @@
 package controllers
 
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.json.Json.toJson
 import play.api.mvc.{Action, Controller}
 
 object ContactBack extends Controller{
+
+  private var contact = fabricator.Contact()
   
-  private val contact = fabricator.Contact()
+  private def checkLanguage(lang: String) = {if (!lang.equals(Language.US.toString)) contact = fabricator.Contact(lang)}
   
-  def name(json: Boolean) = Action {
+  case class Name(name: Map[String, String])
+  case class Address(address: Map[String, String])
+  case class Birthday(birthday: String)
+  
+  implicit def nameFormat = Json.format[Name]
+  implicit def addressFormat = Json.format[Address]
+  implicit def birthdayFormat = Json.format[Birthday]
+
+  implicit val personMapFormat = new Format[Map[String, Object]] {
+
+    def writes(map: Map[String, Object]): JsValue =
+      Json.obj(
+        "name" -> map("name").asInstanceOf[Map[String, String]],
+        "address" -> map("address").asInstanceOf[Map[String, String]],
+        "birthday" -> map("birthday").asInstanceOf[String],
+        "email" -> map("email").asInstanceOf[String],
+        "bsn" -> map("bsn").asInstanceOf[String],
+        "religion" -> map("religion").asInstanceOf[String],
+        "zodiac" -> map("zodiac").asInstanceOf[String],
+        "height" -> map("height").asInstanceOf[String],
+        "weight" -> map("weight").asInstanceOf[String],
+        "blood_type" -> map("blood_type").asInstanceOf[String],
+        "occupation" -> map("occupation").asInstanceOf[String]
+      )
+
+    def reads(jv: JsValue): JsResult[Map[String, Object]] =
+    JsSuccess(Map(
+      "name" -> (jv \ "name").as[Map[String, String]],
+      "address" -> (jv \ "address").as[Map[String, String]],
+      "birthday" -> (jv \ "birthday").as[String],
+      "email" -> (jv \ "email").as[String],
+      "bsn" -> (jv \ "bsn").as[String],
+      "religion" -> (jv \ "religion").as[String],
+      "zodiac" -> (jv \ "zodiac").as[String],
+      "height" -> (jv \ "height").as[String],
+      "weight" -> (jv \ "weight").as[String],
+      "blood_type" -> (jv \ "blood_type").as[String],
+      "occupation" -> (jv \ "occupation").as[String]))
+  }
+  
+  def name(json: Boolean, lang: String) = Action {
+    checkLanguage(lang)
     val name = Map("first_name" -> contact.firstName,
-                   "last_name" -> contact.lastName,   
-                   "full_name" -> contact.fullName(false)) // TODO fix this method when new version of library will appear
-    if (json) Ok(Json.toJson(name)) else Ok(Json.toJson(contact.fullName(false)))
+                   "last_name" -> contact.lastName,
+                   "full_name" -> contact.fullName(false)) // TODO fix this method when new version of library will apper
+    if (json) Ok(toJson(Name(name))) else Ok(toJson(contact.fullName(false)))
   }
-  
-  def birthday(age: Int, format: String,  json: Boolean) = Action{
-    if (json) Ok(Json.toJson(Map("value" -> contact.birthday(age, format)))) else Ok(Json.toJson(contact.birthday(age, format)))
+
+  def birthday(age: Int, format: String,  json: Boolean, lang: String) = Action{
+    checkLanguage(lang)
+    if (json) Ok(toJson(Birthday(contact.birthday(age, format)))) else Ok(toJson(contact.birthday(age, format)))
   }
-  
-  def address(json: Boolean) = Action {
+
+  def address(json: Boolean, lang: String) = Action {
+    checkLanguage(lang)
     val address = Map("phone_number" -> contact.phoneNumber,
                       "street_name" -> contact.streetName,
                       "house_number" -> contact.houseNumber,
@@ -30,10 +76,11 @@ object ContactBack extends Controller{
                       "company" -> contact.company
     )
     val addressString = String.format("%s %s", contact.address, contact.postcode)
-    if (json) Ok(Json.toJson(address)) else Ok(Json.toJson(addressString))
+    if (json) Ok(toJson(Address(address))) else Ok(toJson(addressString))
   }
-  
-  def person(age: Int, metric: Boolean) = Action {
+
+  def person(age: Int, metric: Boolean, lang: String) = Action {
+    checkLanguage(lang)
     val name = Map("first_name" -> contact.firstName,
       "last_name" -> contact.lastName,
       "full_name" -> contact.fullName(false))
@@ -59,10 +106,8 @@ object ContactBack extends Controller{
                      "weight" -> contact.weight,
                      "blood_type" -> contact.bloodType,
                      "occupation" -> contact.occupation
-
     )
-    Ok(Json.toJson(address))
-    
+    Ok(toJson(person))
   }
 
 }
---
+++ app/controllers/Language.scala
@@ -0,0 +1,8 @@
+package controllers
+
+object Language extends Enumeration{
+  type Language = Value
+  val US = Value("us")
+  val DE = Value("de")
+  val NL = Value("nl")
+}
