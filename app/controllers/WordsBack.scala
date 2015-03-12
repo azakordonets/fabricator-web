469628e256b265cbd676678f8677599955b03bb8
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2015-03-12 21:20:37 +0100

    implement WordsBack controller

--- app/controllers/WordsBack.scala
+++ app/controllers/WordsBack.scala
@@ -1,14 +1,34 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
+import play.api.mvc.{Action, Controller}
 
 object WordsBack extends Controller{
 
   private val words = fabricator.Words()
 
-  def getWords(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  case class wordsSeq(words: Seq[String])
+  case class sentencesSeq(sentences: Seq[String])
+  case class paragraphsSeq(paragraphs: Seq[String])
 
-  def paragraph(length: Int, json: Boolean) = play.mvc.Results.TODO
+  implicit def wordsWrites = Json.writes[wordsSeq]
+  implicit def sentencesWrites = Json.writes[sentencesSeq]
+  implicit def paragraphsWrites = Json.writes[paragraphsSeq]
 
-  def sentence(words: Int, json: Boolean) = play.mvc.Results.TODO
+  def getWords(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("word" -> words.word))) else Ok(words.word)
+    } else {
+      if (json) Ok(toJson(wordsSeq(words.words(amount)))) else Ok(toJson(words.words(amount)))
+    }
+  }
+
+  def paragraph(length: Int, json: Boolean) = Action {
+    if (json) Ok(toJson(Map("paragraph" -> words.paragraph(length)))) else Ok(words.paragraph(length))
+  }
+
+  def sentence(wordsAmount: Int, json: Boolean) = Action {
+    if (json) Ok(toJson(Map("sentence" -> words.sentence(wordsAmount)))) else Ok(words.sentence(wordsAmount))
+  }
 }
