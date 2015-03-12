51b43b4a223ae28559c1e04b4301ddd0f92c5fe2
Andrii Zakordonets <andrii.zakordonets@portavita.nl>
2015-03-12 21:10:34 +0100

    implemented MobileBack controller

--- app/controllers/MobileBack.scala
+++ app/controllers/MobileBack.scala
@@ -1,18 +1,58 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.libs.json.Json
+import play.api.libs.json.Json._
+import play.api.mvc.{Action, Controller}
 
 object MobileBack extends Controller{
   
   private val mobile = fabricator.Mobile()
 
-  def android(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  case class tokens(tokens: Seq[String])
+  implicit def tokensWrite = Json.writes[tokens]
 
-  def apple(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  def android(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("token" -> mobile.androidGsmId))) else Ok(mobile.androidGsmId)
+    } else {
+      val androidTokens = Seq.fill(amount)(mobile.androidGsmId)
+      if (json) Ok(toJson(tokens(androidTokens))) else Ok(toJson(androidTokens))
+    }
+  }
 
-  def wp7(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  def apple(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("token" -> mobile.applePushToken))) else Ok(mobile.applePushToken)
+    } else {
+      val appleTokens = Seq.fill(amount)(mobile.applePushToken)
+      if (json) Ok(toJson(tokens(appleTokens))) else Ok(toJson(appleTokens))
+    }
+  }
 
-  def wp8(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  def wp7(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("token" -> mobile.wp7_anid))) else Ok(mobile.wp7_anid)
+    } else {
+      val wp7Tokens = Seq.fill(amount)(mobile.wp7_anid)
+      if (json) Ok(toJson(tokens(wp7Tokens))) else Ok(toJson(wp7Tokens))
+    }
+  }
 
-  def blackberry(json: Boolean, amount: Int) = play.mvc.Results.TODO
+  def wp8(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("token" -> mobile.wp8_anid2))) else Ok(mobile.wp8_anid2)
+    } else {
+      val wp8Tokens = Seq.fill(amount)(mobile.wp8_anid2)
+      if (json) Ok(toJson(tokens(wp8Tokens))) else Ok(toJson(wp8Tokens))
+    }
+  }
+
+  def blackberry(json: Boolean, amount: Int) = Action {
+    if (amount == 1) {
+      if (json) Ok(toJson(Map("token" -> mobile.blackBerryPin))) else Ok(mobile.blackBerryPin)
+    } else {
+      val blackBerryTokens = Seq.fill(amount)(mobile.blackBerryPin)
+      if (json) Ok(toJson(tokens(blackBerryTokens))) else Ok(toJson(blackBerryTokens))
+    }
+  }
 }
